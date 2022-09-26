cea18e0efea92034ce6907c858844c552dd53591
Tushar <tusharmahajan9898@gmail.com>
2022-09-26 20:24:07 +0530

    Initial commit

---
+++ app/src/main/java/tushar/project/boomboomblast/Missile.java
@@ -0,0 +1,23 @@
+package tushar.project.boomboomblast;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+public class Missile {
+    int x, y;
+    int mVelocity;
+    Bitmap missile;
+    public Missile(Context context){
+        missile = BitmapFactory.decodeResource(context.getResources(), R.drawable.missile);
+        x = GameView.dWidth/2 - getMissileWidth()/2;
+        y = GameView.dHeight - GameView.tankHeight - getMissileHeight()/2;
+        mVelocity = 50;
+    }
+    public int getMissileWidth(){
+        return missile.getWidth();
+    }
+    public int getMissileHeight(){
+        return missile.getHeight();
+    }
+}
---
+++ app/src/main/java/tushar/project/boomboomblast/StartGame.java
@@ -0,0 +1,24 @@
+package tushar.project.boomboomblast;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.annotation.Nullable;
+
+
+public class StartGame extends Activity {
+
+    GameView gameView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        gameView = new GameView(this);
+        setContentView(gameView);
+    }
+}
